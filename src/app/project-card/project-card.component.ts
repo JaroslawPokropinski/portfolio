f8a64b64e95d7382765390c777cc01efbe557c2a
Jarosław Pokropiński <jarek.pokropinski@gmail.com>
2021-09-21 01:17:14 +0200

    first commit

---
+++ src/app/project-card/project-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.scss']
+})
+export class ProjectCardComponent {
+  @Input() image = '';
+  @Input() title = '';
+  @Input() description = '';
+  @Input() source = '';
+  @Input() applink = '';
+}
